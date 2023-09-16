d7c729bc1ef869e252ed51b65a66f1590752f710
Xuan Truong <xtruongtdo2003@outlook.com>
2023-09-16 22:05:59 +0700

    Tao khung project
    Tạo khung project

---
+++ src/main/java/com/example/sashoes/SaShoesApplication.java
@@ -0,0 +1,13 @@
+package com.example.sashoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaShoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaShoesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sashoes/SaShoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sashoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaShoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
